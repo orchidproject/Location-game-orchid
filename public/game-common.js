9c020a4fe35884818432f8c7e9f346c7cf30cd16
joelfischer <jef@cs.nott.ac.uk>
2012-03-08 18:10:53 +0000

    fixed Map bug

--- public/game-common.js
+++ public/game-common.js
@@ -118,3 +118,15 @@ function getTaskIcon() {
 	return icon;
 }
 
+
+var GameMap = {
+	fitToRadius: function(radius) {
+	  var center = map.getCenter();
+	  var topMiddle = google.maps.geometry.spherical.computeOffset(center, radius, 0);
+	  var bottomMiddle = google.maps.geometry.spherical.computeOffset(center, radius, 180);
+	  var bounds = new google.maps.LatLngBounds();
+	  bounds.extend(topMiddle);
+	  bounds.extend(bottomMiddle);
+	  map.fitBounds(bounds);
+	}
+}
