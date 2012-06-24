98ed42a648f318ad5458436d7bb720367352a6b7
Joel onHolt <jef@cs.nott.ac.uk>
2012-06-24 17:05:49 +0100

    f

--- main-runtime.js
+++ main-runtime.js
@@ -1,6 +1,6 @@
-var database="jtruck_db";
-var db_username="jtruck";
-var db_password="jtruck";var sys = require('util');
+var database="wxj";
+var db_username="wxj";
+var db_password="t5*m3$A";var sys = require('util');
 var url = require('url');
 
 // Load the node-router library by creationix
@@ -12,11 +12,22 @@ var fs = require('fs');
 
 //log system 
 function write_log(game_id,data){
+	var time = new Date();
+	data.time_stamp = time.getTime();
     var log = fs.createWriteStream('logs/log-'+game_id, {'flags': 'a'});
-    var time = new Date();
-    data.time_stamp = time.getTime();
-    log.write(JSON.stringify(data)+"\n");
+	log.write(JSON.stringify(data)+"\n");
     log.end();
+    
+    //thread safe? oh js should be single threaded.
+    /*fs.openSync('logs/log-'+game_id, 'a', 0777, function( err, id ) {
+    					if (err) throw err;
+                         fs.writeSync( id, JSON.stringify(data)+"\n", null, 'utf8', function(err, written){
+                         	 if (err) throw err;
+                             fs.close(id, function(){
+                                  console.log("write to log:" + JSON.stringify(data)+"\n");
+                             });
+                         });
+                     });*/
 }
 
 
@@ -35,19 +46,27 @@ http.post("/broadcast", function (request, response) {
 	request.addListener("end", function() {
         var ob=JSON.parse(request.content);
         content=JSON.parse(ob.data);
-        content.data["ackid"]=ackid++;
+        ackid++;
+        content.data["ackid"]=ackid;
         var channel=content.channel;console.log(channel);
         var users=content.users;
         
         io.sockets.in(channel).emit('data', content.data);
+        
         write_log(channel,content.data);
         
         
         //send to indvidual users
         if(users!=null){
-        	var user;
-        	for(user in users){
-        		io.sockets.socket(sessionTable[user]).emit('data', content.data);
+        	
+        	for(id in users){
+        		if (sessionTable[users[id]] != null){
+        			console.log["send to user " +  sessionTable[id]]
+        			io.sockets.socket(sessionTable[users[id]]).emit('data', content.data);
+        		}else
+        		{
+        			console.log(users[id] + " not found in session table");
+        		}
         	}
         }
     });
@@ -88,8 +107,6 @@ http.post("/push_log", function (request, response){
 
 // Listen on port 8080 on localhost
 http.listen(process.argv[3], "0.0.0.0");
-
-
 
 //work around, cos no geo library exists for node.js. 
 /*
@@ -156,9 +173,6 @@ function get_game_status(game_layer_id,callback){
    
 }
 
-
-
-
 function insert_request(request){
     client.query(
     'INSERT INTO requests'
@@ -222,7 +236,19 @@ io.sockets.on('connection', function (socket) {
   });
   
   socket.on('ack', function (data) {
-  	 write_log(data.channel,data);
+  	 console.log('ack received ' + data);
+  	 write_log("ack-"+data.channel,data);
+  	 
+  });
+  
+  socket.on('disconnect', function () { 
+  	//unregister
+  	for (i in sessionTable){
+  		if(sessionTable[i]==socket.id){
+  			delete sessionTable[i];
+  			console.log("client unregistered");
+  		}
+  	}
   });
   
   //SINGLE location push
@@ -240,19 +266,22 @@ io.sockets.on('connection', function (socket) {
         //if(is_active==0){
         if(true){
             update_location(data.latitude,data.longitude,data.player_id);
-            data[ackid]=ackid++;
-            io.sockets.in(channel).emit('data', {location:data});
-            write_log(channel,{location:data});
+            //data["ackid"]=ackid++;
+            ackid++;
+            io.sockets.in(channel).emit('data', {"location":data,"ackid":ackid});
+            write_log(channel,{"location":data,"ackid":ackid});
         
         }
         else{
             console.log("game not active, broadcast blocked");
         }
     });
-   
-
     
-  });
+    //test from webview 
+    socket.on('message', function (data) {
+  	 	console.log('message' + data);
+  	});
+});
   
   /*
   socket.on('action-push', function (data) {
