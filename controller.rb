8e1c5bb4220ddbfcacb906b159be9cacddb51d3c
wenchaojiang <jiang.wenchao.goo@googlemail.com>
2012-10-25 16:10:46 +0100

    test improvement

--- controller.rb
+++ controller.rb
@@ -698,8 +698,8 @@ end
         	count=0
         	#6 sec waiting, lett clients get ready
         	sleep 6
-        	puts g.layer_id
-        	puts Game
+        	puts "game_id #{g.layer_id}"
+        	puts "game_ob #{Game}"
             g= Game.get g.layer_id
             while(g.is_active==0) do
                
