72ec1344ef8424d02bb69e5dbb850cef8c55d5b5
wenchaojiang <jiang.wenchao.goo@googlemail.com>
2012-11-18 20:40:24 +0000

    user icon

--- public/game-common.js
+++ public/game-common.js
@@ -187,7 +187,7 @@ function receivePlayerData(data) {
 		            marker: new google.maps.Marker({
 		                position: new google.maps.LatLng(data.latitude, data.longitude),
 		                map: map,
-		                icon: markerIcon,
+		                icon: new google.maps.MarkerImage("/img/blue_dot.png", playerIconSize, playerIconOrigin, playerIconAnchor);,
 		                visible: true
 		            })
 		        };
