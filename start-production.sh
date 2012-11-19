aa70bb2a43e868f3b4bd3dd465aa5bd08cde7cc4
wenchaojiang <jiang.wenchao.goo@googlemail.com>
2012-11-19 16:12:29 +0000

    start server script

--- start-production.sh
+++ start-production.sh
@@ -1,7 +1,7 @@
 sh start-server.sh &> output &
 
-echo "kill "$!"&&" > kill.sh
+echo "kill "$!" &&" > kill.sh
 
 sh start-node-server.sh &> output1 & 
 
-echo "kill "$!" >> kill.sh
+echo "kill "$! >> kill.sh
