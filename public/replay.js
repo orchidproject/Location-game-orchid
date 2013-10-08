1c819d0eac526a8902a66d1c77cc792fdc34dce8
wenchaojiang <jiang.wenchao.goo@googlemail.com>
2013-10-08 09:28:23 +0100

    analysis script

--- public/replay.js
+++ public/replay.js
@@ -180,16 +180,140 @@ function process_data(data){
         }
         
         if(typeof data.task != "undefined"){
+		check_task(data.task);
                 receiveTaskData(data.task);
         }
 	
         if(typeof data.instructions != "undefined"){
                 receiveInstructionDataV3(data.instructions[0]);
+		ana_push_instructions(data.instructions[0]);
         }
-	
+
+	if(typeof data["ack-instruction"] != "undefined"){
+		var ack = data["ack-instruction"]
+		ana_push_instrucition_ack(ack);
+        }
+
         
 }
+//analysis
+var metrics = { finish_accepted: 0, task_allocation : 0 , rejection : 0 , acceptance: 0, no_decision : 0}; 
+var ana_instructions = [];
+function ana_push_instructions (ins){
+	var pre_ins = get_ins_by_player(ins.player_id); 
+	if( pre_ins==null){
+		ana_instructions.push(ins); 
+	}
+	else{
+		//delete previous 
+		ana_remove(pre_ins.player_id);	
+		ana_instructions.push(ins); 
+		
+	}
+	//get teammate
+	var team_ins = get_ins_by_player(ins.teammate);	
+	if (team_ins != null) {
+		//check new assigment completed
+		if(team_ins.teammate == ins.player_id && team_ins.task == ins.task){
+			metrics.task_allocation++;		
+	//		alert("new assignment " + metrics.task_allocation);
+		}
+	}
+}
 
+function ana_push_instrucition_ack(ack){
+	var ins = get_ins_by_player(ack.player_id);
+	ins.status = ack.status;
+	//need to confirm that the assignment is validated	
+	var teammate = get_ins_by_player(ins.teammate);
+	if(teammate!=null){ 
+		var p1 = players[ins.player_id]
+		var p2 = players[ins.teammate];
+		var distance = ana_get_distance(p1.marker.position,p2.marker.position);
+		if(ack.status == 2 && teammate.status == 2){
+			metrics.acceptance++;
+			ana_accept_distance+= distance;
+			var avg = ana_accept_distance/metrics.acceptance; 
+			alert("accept " + metrics.acceptance + " avg_distance: " + avg);
+		}
+		else if (ack.status == 3 && teammate.status != 3){
+			metrics.rejection++;
+			ana_rejection_distance+= distance;
+			var avg = ana_rejection_distance/metrics.rejection; 
+			alert("rejection " + metrics.rejection + " avg_distance: " + avg); 
+		}	
+	}
+	
+	  
+} 
+var ana_rejection_distance = 0;
+var ana_accept_distance = 0;
+
+
+function get_ins_by_player(id){
+	var ins = null;
+	$(ana_instructions).each(function(index,value){
+		if (value.player_id == id){	
+			ins = value			
+		}
+	});	
+	return ins;
+
+}
+
+function get_ins_by_task(id){
+	var res = []; 
+	$(ana_instructions).each(function(index,value){
+		if (value.task == id){	
+			res.push(value);	
+		}
+	});
+	return res;
+
+}
+
+function ana_get_task_by_id(id){
+	var t = null;
+	$(tasks).each(function(index,value){
+		if (value.id == id){	
+			t = value;			
+		}
+	});	
+	return t;
+
+}
+
+function check_task(task){
+	var t = ana_get_task_by_id(task.id);
+	if(t.state ==1 && task.state == 2){
+		var ins = get_ins_by_task(task.id);
+		
+		if(ins.length == 2 && ins[0].status == 2 && ins[1].status ==2){
+			metrics.finish_accepted++;	
+			//alert("accepted and finished " + metrics.finish_accepted);
+		}
+		else{
+			//alert("just finished ");
+		}
+	}
+}
+
+function ana_remove(id){
+	var i = -1;
+	$(ana_instructions).each(function(index,value){
+		if (value.player_id == id){	
+			i = index; 
+		}
+	});	
+	ana_instructions.splice(i,1);
+
+}
+
+function ana_get_distance(l1,l2){
+	return google.maps.geometry.spherical.computeDistanceBetween(l1, l2);	  
+}
+
+//-------------------
 function draw_stroke(p1, p2){
 
 }
