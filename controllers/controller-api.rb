b4a6305c0e0e8d927c8296750a9ce31e12d0c9c1
wenchaojiang <jiang.wenchao.goo@googlemail.com>
2014-08-26 10:29:50 +0100

    AO initial

--- controllers/controller-api.rb
+++ controllers/controller-api.rb
@@ -14,4 +14,23 @@ class Controller < Sinatra::Base
 	    snapshot @game
     
 	end
+
+	post '/game/:layer_id/find_target'  do
+		data = JSON.parse(request.body.read)
+		t = Task.get(data["target_id"])
+		puts t.state 
+		puts Task::State::IDLE
+		if t.nil?
+			return {state:"error", msg:"no target found"}.to_json
+		elsif t.state == Task::State::UNSEEN
+			t.state = Task::State::IDLE
+			t.save
+			t.broadcast(socketIO);
+		else
+			return {state:"error", msg:"target already discovered"}.to_json
+		end
+
+		return {state:"ok"}.to_json
+	end
+
 end
