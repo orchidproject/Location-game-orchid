48e2b95d353914311e167ce0ea5e0d48b15efeae
wenchaojiang <jiang.wenchao.goo@googlemail.com>
2014-09-07 16:05:20 +0100

    configuration

--- lib/scripts/load_game.rb
+++ lib/scripts/load_game.rb
@@ -1,4 +1,4 @@
-data = File.read("./lib/scripts/session/formal-test.json")
+data = File.read("./lib/scripts/session/formal.json")
 data = JSON.parse(data)
 game = Game.create(:latitude => 52.952617, :longitude=> -1.188639,
 	:radius => 1000,
@@ -32,7 +32,7 @@ data["tasks"].each do |p|
 		:latitude => p["latitude"] ,
 		:longitude =>p["longitude"],
 		:type =>p["type"],
-		:state =>4,
+		:state => p["state"],
 		:players => p["players"],
 		:shared_id => count
 	) 
